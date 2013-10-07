f16217536da1384bf80d91a5197c95888367c2c0
Chris2408 <christophe.nickel@students.hevs.ch>
2013-10-07 11:34:15 -0700

    Correction bugg

--- app/cache/dev/twig/8c/dc/0a8241c8515c34856e957d3a0780.php
+++ app/cache/dev/twig/8c/dc/0a8241c8515c34856e957d3a0780.php
@@ -16,20 +16,13 @@ class __TwigTemplate_8cdc0a8241c8515c34856e957d3a0780 extends Twig_Template
     protected function doDisplay(array $context, array $blocks = array())
     {
         // line 1
-        echo "Hello ";
-        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
-        echo "!
+        echo "Hello !
 ";
     }
 
     public function getTemplateName()
     {
         return "bccQuizzBundle:Default:index.html.twig";
-    }
-
-    public function isTraitable()
-    {
-        return false;
     }
 
     public function getDebugInfo()
